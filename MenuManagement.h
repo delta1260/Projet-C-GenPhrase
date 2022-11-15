75190f2d7242e88813b8974646fe5165a6badf09
delta1260 <98814233+delta1260@users.noreply.github.com>
2022-11-15 16:44:30 +0100

    implémentation menu

---
+++ MenuManagement.h
@@ -0,0 +1,14 @@
+//
+// Created by Mathys on 15/11/2022.
+//
+
+#ifndef PROJET_C_FINALE_MENUMANAGEMENT_H
+#define PROJET_C_FINALE_MENUMANAGEMENT_H
+
+int estDansChoix(char * nombre,char * choixList);
+char ask(char* message,int mode, char * choiceList);
+void afficherMenu();
+void nettoyerEcran();
+void GenererPhrase();
+
+#endif //PROJET_C_FINALE_MENUMANAGEMENT_H
