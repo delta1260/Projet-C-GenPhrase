3d81435ba44b629bc1d971c5b8981c88f3256a11
Ekliptus <98977762+Ecliptus@users.noreply.github.com>
2022-11-20 22:30:15 +0100

    Update tree.c

--- tree.c
+++ tree.c
@@ -4,6 +4,7 @@
 
 #include "tree.h"
 
+//création d'un nouveau noeud
 p_node newNode(char c) {
     p_node new = malloc(sizeof(t_node));
     new->letter = c;
@@ -13,9 +14,10 @@ p_node newNode(char c) {
     return new;
 }
 
+//création des arbres principaux
 allTree initTree() {
     allTree tree;
-    p_node new = newNode(' ');
+    p_node new = newNode(' '); // le char ' ' n'est jamais utilisé mais est nécéssaire au bon fonctionnement du code
     tree.nom = new;
     new = newNode(' ');
     tree.verbe = new;
@@ -26,10 +28,11 @@ allTree initTree() {
     return tree;
 }
 
+// fonction récursive pour mettre les formes de bases dans les arbres
 listeFlechi* addWord(p_node pn, char* word) {
-    if (word[0] != '\0') {
+    if (word[0] != '\0') {//verifie s'il reste des lettres dans le mot
         int n = 1;
-        while (pn->nbNext >= n && pn->next[n - 1]->letter != word[0]) {
+        while (pn->nbNext >= n && pn->next[n - 1]->letter != word[0]) {// verifie si la lettre se trouve deja dans les suivants
             n++;
         }
         if (pn->nbNext == 0 || pn->nbNext < n) {
@@ -37,19 +40,19 @@ listeFlechi* addWord(p_node pn, char* word) {
             p_node new = newNode(word[0]);
             pn->next[n-1] = new;
             pn->nbNext++;
-            return addWord(new, word + 1);
+            return addWord(new, word + 1);// continue la récursivité avec nouveau noeud contenant la lettre actuelle et le mot sa première lettre
         }
         else
-            return addWord(pn->next[n - 1], word + 1);
+            return addWord(pn->next[n - 1], word + 1);// continue la récursivité avec le noeud la lettre actuelle et le mot sans celle-ci
     }
     else {
-        if (pn->liste == NULL) {
+        if (pn->liste == NULL) {// liste de mot flechi inexistante ?
             listeFlechi* list = malloc(sizeof(listeFlechi));
             list->nbmots = 0;
             pn->liste = list;
-            return list;
+            return list;// retourne le pointeur de liste de mot flechi nouvellement créé
         }
         else
-            return pn->liste;
+            return pn->liste;//// retourne le pointeur de liste de mot flechi existante
     }
 }
